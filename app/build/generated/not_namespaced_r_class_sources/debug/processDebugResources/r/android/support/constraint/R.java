b41b3cff6c74a5ac931f622d4719d6de33db9974
Rosaverde <freez@interia.eu>
2019-05-25 21:29:19 +0000

    Adding JAVA FINISH

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f070041;
         public static final int invisible = 0x7f07004d;
         public static final int left = 0x7f07004f;
-        public static final int packed = 0x7f07005d;
-        public static final int parent = 0x7f07005e;
-        public static final int percent = 0x7f070060;
-        public static final int right = 0x7f070064;
-        public static final int spread = 0x7f07007c;
-        public static final int spread_inside = 0x7f07007d;
-        public static final int start = 0x7f070082;
-        public static final int top = 0x7f070099;
-        public static final int wrap = 0x7f07009f;
+        public static final int packed = 0x7f07005f;
+        public static final int parent = 0x7f070060;
+        public static final int percent = 0x7f070062;
+        public static final int right = 0x7f070066;
+        public static final int spread = 0x7f07007f;
+        public static final int spread_inside = 0x7f070080;
+        public static final int start = 0x7f070085;
+        public static final int top = 0x7f07009c;
+        public static final int wrap = 0x7f0700a2;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/gtr/dices/MainActivity.java
+++ app/src/main/java/com/gtr/dices/MainActivity.java
@@ -1,13 +1,173 @@
 package com.gtr.dices;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView leftDice;
+    ImageView rightDice;
+    Button newGameButton;
+    Button rollButton ;
+    Button holdButton;
+    TextView mPlayer1_ScoreTextView;
+    TextView mPlayer2_ScoreTextView;
+    TextView mPlayer1_RoundScore;
+    TextView mPlayer2_RoundScore;
+    int scoresP1 = 0;
+    int scoresP2 = 0;
+    int roundScores;
+    int activePlayer = 0;
+
+    final int[] diceArray = {R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        newGameButton = findViewById(R.id.button_NewGame);
+        rollButton  = findViewById(R.id.button_Roll);
+        holdButton = findViewById(R.id.button_Hold);
+        mPlayer1_ScoreTextView = findViewById(R.id.textView_currentScorePlayer1);
+        mPlayer2_ScoreTextView = findViewById(R.id.textView_currentScorePlayer2);
+        mPlayer1_RoundScore = findViewById(R.id.textView_RoundScorePlayer1);
+        mPlayer2_RoundScore = findViewById(R.id.textView_RoundScorePlayer2);
+
+        leftDice = findViewById(R.id.image_leftDice);
+        rightDice = findViewById(R.id.image_rightDice);
+
+
+
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+                leftDice.setImageResource(diceArray[number]);
+
+                int number1 = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(diceArray[number1]);
+
+                roundScores +=2+ number + number1;
+                if(number == 0 || number1 == 0)
+                {
+                    Toast.makeText(getApplicationContext(),"You Hit 1 END TURN",Toast.LENGTH_SHORT).show();
+                    roundScores = 0;
+                    mPlayer1_ScoreTextView.setText("0");
+                    mPlayer2_ScoreTextView.setText("0");
+                    if(activePlayer == 0)
+                    {
+                        activePlayer = 1;
+                    }
+                    else
+                    {
+                        activePlayer = 0;
+                    }
+                }
+
+                if(activePlayer == 0) {
+                    mPlayer1_ScoreTextView.setText(Integer.toString(roundScores));
+                } else {
+                    mPlayer2_ScoreTextView.setText(Integer.toString(roundScores));
+                }
+
+            }
+        });
+
+        holdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(activePlayer == 0)
+                {
+                    scoresP1 += roundScores;
+                    mPlayer1_RoundScore.setText(Integer.toString(scoresP1));
+                    mPlayer1_ScoreTextView.setText(Integer.toString(0));
+                    roundScores = 0;
+                    if(scoresP1 > 100)
+                    {
+                        Winner();
+                    }
+                    else
+                        {
+                            activePlayer = 1;
+
+                        }
+                }
+                else
+                {
+                    scoresP2 += roundScores;
+                    mPlayer2_RoundScore.setText(Integer.toString(scoresP2));
+                    mPlayer2_ScoreTextView.setText(Integer.toString(0));
+                    roundScores = 0;
+
+                    if(scoresP2 > 100)
+                    {
+                        Winner();
+                    }
+                    else
+                    {
+                        activePlayer = 0;
+                    }
+                }
+            }
+        });
+
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newTurn();
+            }
+        });
+
+    }
+
+    public void Winner()
+    {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("WINNER");
+        alert.setMessage("Player " + (activePlayer + 1) );
+        alert.setCancelable(false);
+        alert.setPositiveButton("END GAME", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                newTurn();
+            }
+        });
+        alert.show();
+    }
+    public void newTurn()
+    {
+        activePlayer = 0;
+        scoresP1 = 0;
+        scoresP2 = 0;
+        roundScores = 0;
+        mPlayer1_ScoreTextView.setText("0");
+        mPlayer2_ScoreTextView.setText("0");
+        mPlayer1_RoundScore.setText("0");
+        mPlayer2_RoundScore.setText("0");
+        leftDice.setImageResource(diceArray[5]);
+        rightDice.setImageResource(diceArray[5]);
     }
 }
